6ad3a9fc725612d91ce13c046e02d036e7c5cc7b
ThiagoUff <thiago.castro.c@hotmail.com>
2022-06-19 21:13:35 -0300

    função lambda criada

--- app/hate-speech-model.py
+++ app/hate-speech-model.py
@@ -1,72 +1,70 @@
 import pandas as pd
 import numpy as np
-import nltk
+import math
 import spacy
 import pickle
 import string
+import boto3
+import os
+import json
+import re
 
 from sklearn.linear_model import LogisticRegression
+from sklearn.feature_extraction.text import CountVectorizer
 
 #stop_words = spacy.lang.pt.stop_words.STOP_WORDS
 punctuations = string.punctuation
 nlp = spacy.load('pt_core_news_sm')
 
 def spacy_tokenizer(sentence):
-
-    # Creating our token object, which is used to create documents with linguistic annotations.
     mytokens = nlp(sentence)
-
-    # Lemmatizing each token and converting each token into lowercase
-    
     mytokens = [word.lemma_.lower().strip() if word.lemma_ != "-PRON-" else word.lower_ for word in mytokens]
-    #mytokens = [ word.lemma_.lower().strip() for word in mytokens ]
-
-    # Removing stop words
-    #mytokens = [word for word in mytokens if word not in stop_words]
-
-    # Removing punctuations
     mytokens = [str(word) for word in mytokens if str(word) not in punctuations]
-    # return preprocessed list of tokens
-    return mytokens
+    return " ".join(mytokens)
 
+def removeChars(sentence):
+    sentence = re.sub(r'(@\w*)', '', sentence)
+    sentence = re.sub(r"http\S+", "", sentence)
+    sentence = re.sub(r'#\w+', "", sentence)
+    sentence = re.sub(r'( +)', " ", sentence)
+    sentence = sentence.strip()
+    sentence = sentence.lower()
 
-def removeChars(dataFrame):
-    # Remove @ tags
-    dataFrame.txt = dataFrame.txt.str.replace(r'(@\w*)', '', regex=True)
+def lambda_handler(event, context):
+    # Parse input
+    body = event['body']
+    input = json.loads(body)['data']
+    input = str(input) 
+   
 
-    # Remove URL
-    dataFrame.txt = dataFrame.txt.str.replace(r"http\S+", "", regex=True)
+    # Download pickled model from S3 and unpickle
+    s3.download_file(s3_bucket, model_name, model_file_path)
+    s3.download_file(s3_bucket, vector_name, vector_file_path)
+   
+    with open(model_file_path, 'rb') as f:
+        model = pickle.load(f) 
+        
+    with open(vector_file_path, 'rb') as f:
+        freq_vector = pickle.load(f)  
+   
+    removeChars(input)
+    input = spacy_tokenizer(input)
+    d = {'corpus': [input]}
+    df = pd.DataFrame(data=d)
+    
+    input = freq_vector.transform(df.corpus)
 
-    # Remove # tag
-    dataFrame.txt = dataFrame.txt.str.replace(r'#\w+', "", regex=True)
-    # comp_df.tweet = comp_df.tweet.str.replace(r'#+',"")
-
-    # Remove all non-character
-    # comp_df.tweet = comp_df.tweet.str.replace(r"[^a-zA-Z ]","")
-
-    # Remove extra space
-    dataFrame.txt = dataFrame.txt.str.replace(r'( +)', " ", regex=True)
-    dataFrame.txt = dataFrame.txt.str.strip()
-
-    # Change to lowercase
-    dataFrame.txt = dataFrame.txt.str.lower()
-
-
-insults_df = pd.read_json('dataset/df_dataset.json')
-
-removeChars(insults_df)
-insults_df['corpus'] = [spacy_tokenizer(text) for text in insults_df.txt]
-
-x_train = insults_df['corpus'].squeeze()
-y_train = insults_df['has_anger'].squeeze()
-
-#freq_vector = CountVectorizer(min_df=2, ngram_range=(1, 2)).fit(insults_df.corpus)
-
-classifier = LogisticRegression(max_iter=500)
-
-classifier.fit(x_train, y_train)
-
-y_pred_train = classifier.predict(x_train)
-
-
-pickle.dump( classifier, open( "pickled_model.p", "wb" ) )
+    prediction = model.predict(input)[0]   
+    return {
+        "statusCode": 200,
+        "body": json.dumps({
+            "prediction": str(prediction),
+        }),
+    }
+    
+s3 = boto3.client('s3')
+s3_bucket = os.environ['s3_bucket']
+model_name = os.environ['model_name']
+vector_name = os.environ['vector_name']
+model_file_path = '/tmp/' + model_name
+vector_file_path = '/tmp/' + vector_name
